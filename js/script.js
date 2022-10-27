1a573af035620040b2d5eef2b289e66486d6d4bd
Denis <hallo-boy@mail.ru>
2022-10-27 08:58:57 +0300

    header fixed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-let navigation = document.querySelector('.header__nav'); // Инициализируем блок навигации
-
-document.onscroll = function() {
-  if (document.scrollTop >= navigation.offsetHeight) { // Если началась прокрутка, то...
-    navigation.classList.add('stick'); // Добавляем класс со стилем со свойством position: fixed
-    navigation.classList.remove('static'); // Удаляем класс со стилем со свойством position: static
-  } else { // Иначе...
-    navigation.classList.add('static'); // Добавляем класс со стилем со свойством position: fixed
-    navigation.classList.remove('fixed'); // Удаляем класс со стилем со свойством position: static
-  };
-
-  if (document.scrollTop == document.body.offsetHeight - document.body.clientHeight) { // Если дошли до низа, то...
-    navigation.classList.add('hidden'); // Добавляем класс со стилем со свойством display: none
-  } else { // Иначе...
-    navigation.classList.remove('hidden'); // Удаляем класс со стилем со свойством display: none
-  };
-}
+window.onscroll = function showNav() {
+  let nav = document.querySelector(".header__nav");
+  let basket = document.querySelector(".header__basket");
+  
+  if (window.pageYOffset > 100) {
+    nav.classList.add("header__nav-fixed");
+    basket.classList.add("header__basket-fixed");
+  } else {
+    nav.classList.remove("header__nav-fixed");
+    basket.classList.remove("header__basket-fixed");
+  }
+};
