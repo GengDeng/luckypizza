c52eff61d4c6a8e557aec316fd9852d09a769a5c
Denis <hallo-boy@mail.ru>
2022-10-28 08:42:14 +0300

    nav

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 window.onscroll = function showNav() {
-  let escort = document.querySelector(".header__escort");
- let nav = document.querySelector(".header__nav");
- let basket = document.querySelector(".header__basket");
+  let nav = document.querySelector(".header__nav");
+  let basket = document.querySelector(".header__basket");
+  
   if (window.pageYOffset > 100) {
-    escort.classList.add("header__escort-fixed");
-    nav.style.margin = "auto";
-    basket.style.margin = "auto";
+    nav.classList.add("header__nav-fixed");
+    basket.classList.add("header__basket-fixed");
   } else {
-    escort.classList.remove("header__escort-fixed");
-    nav.style.margin = "auto auto 0 auto";
-    nav.style.marginTop = "auto";
+    nav.classList.remove("header__nav-fixed");
+    basket.classList.remove("header__basket-fixed");
   }
 };
